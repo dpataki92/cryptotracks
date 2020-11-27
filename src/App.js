bdcb9851c56829ef320f5e1299b9a892257ea105
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-27 22:00:10 +0100

    add initial routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import CoinDetails from './coinDetails.js';
+import CoinList from './coinList.js';
 
 const App = () => {
     return(
         <div>
-            App
+            <Router>
+                <Route exact path="/" component={CoinList}/>
+            </Router>
         </div>
     )
 }
