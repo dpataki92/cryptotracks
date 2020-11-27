f06fb71e495ee6d05d7ce5c648e7870006c960b4
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-27 22:31:04 +0100

    move header rendering to App component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import CoinDetails from './components/coinDetails.js';
 import CoinList from './components/coinList.js';
 import './App.css';
+import Header from './header.js';
 
 
 const App = () => {
     return(
         <div>
             <Router>
+                <Header />
                 <Route exact path="/" component={CoinList}/>
             </Router>
         </div>
--- src/components/coinList.js
+++ src/components/coinList.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from './header.js';
 
 const CoinList = () => {
     return (
         <div>
-            <Header />
+            
         </div>
     )
 }
