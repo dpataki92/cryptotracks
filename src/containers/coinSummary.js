76ccc5dd630f6fd3924c68d5a9f2fabc8df7c895
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-28 17:40:24 +0100

    make CoinSummary render AddCoinButton and CoinList components

--- src/containers/coinSummary.js
+++ src/containers/coinSummary.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import AddCoinButton from './components/addCoinButton.js';
+import CoinList from './coinList.js';
 
 const CoinSummary = () => {
     return (
         <div>
-            
+            <AddCoinButton />
+            <CoinList />
         </div>
     )
 }
