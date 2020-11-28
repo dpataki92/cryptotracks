1a2cd34ef3b7a089b89f5e0315ae3d0104538cc1
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-28 19:06:38 +0100

    set up initial fetch api flow

--- src/containers/coinSummary.js
+++ src/containers/coinSummary.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import AddCoinButton from './components/addCoinButton.js';
-import CoinList from './coinList.js';
+import React, { useEffect, useState } from 'react';
+import AddCoinButton from '../components/addCoinButton.js';
+import CoinList from '../components/coinList.js';
+import coinGecko from '../apis/coinGecko.js';
 
 const CoinSummary = () => {
+
+    const [coins, setCoins] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await coinGecko.get('/coins/markets', {
+                params: {
+                    vs_currency: 'usd',
+                    ids: "bitcoin,ethereum"
+                }
+            });
+
+            console.log(response.data);
+
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div>
             <AddCoinButton />
