d079b0fd7198ff1830fb36faeabe551f3bffd8fb
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-29 09:27:19 +0100

    handle loading on coin details page

--- src/containers/coinDetails.js
+++ src/containers/coinDetails.js
@@ -7,9 +7,13 @@ import coinGecko from '../apis/coinGecko.js';
 const CoinDetails = () => {
     const { id } = useParams();
     const [coinData, setCoinData] = useState([]);
+    const [isLoading, setIsLoading] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
+
+            setIsLoading(true);
+
             const [day, week, year, detail] = await Promise.all([
                 coinGecko.get(`/coins/${id}/market_chart`, {
                 params: {
@@ -39,6 +43,7 @@ const CoinDetails = () => {
                 year: year.data.prices,
                 detail: detail.data[0]
             });
+            setIsLoading(false)
             
         }
         
@@ -47,12 +52,16 @@ const CoinDetails = () => {
     }, [])
 
     const coinRender = () => {
-        return(
-            <div className="coinlist">
-                <PriceChart />
-                <CoinData />
-            </div>
-        )
+        if (isLoading) {
+            return <div><i className="fas fa-coins"></i> Your coin is loading...</div>
+        } else {
+            return(
+                <div className="coinlist">
+                    <PriceChart />
+                    <CoinData />
+                </div>
+            )
+        }
     }
     
     return coinRender;
