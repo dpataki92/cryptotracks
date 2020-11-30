d6cda1e5083dddf60b7425533614ec0af122d38d
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-30 18:28:39 +0100

    update decideSort function

--- src/helpers/updateCompareCanvas.js
+++ src/helpers/updateCompareCanvas.js
@@ -6,10 +6,12 @@ export const decideSort = (arr, condition = null) => {
     }
     else if (condition === "market_cap") {
         return insertionSortMarketCap(arr).slice(0,5);
-    } else if (condition === "high_24h") {
-        return bubbleSort24HourHigh(arr).slice(-5);
-    } else if (condition === "total_volume") {
-        return selectionSort24HourVolume(arr).slice(0,5);
+    } 
+    else if (condition === "high_24h") {
+        return bubbleSort24HourHigh(arr).slice(-5).reverse();
+    } 
+    else if (condition === "total_volume") {
+        return selectionSort24HourVolume(arr).slice(0,5).reverse();
     }
 }
 
